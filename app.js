e46323e017dd4633309b6dcfb98ca4f40bd9feec
Maksim Tarhunakau <maksim_tarhunakau@epam.com>
2015-11-03 09:37:59 +0300

    Changing routes and adding JADE

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 /**
  * Module dependencies.
  */
-
 var express = require('express')
 var routes = require('./routes');
 var app = express();
@@ -11,10 +10,9 @@ var port = 3000;
 //mongoose.connect('mongodb://admin:admin@ds047504.mongolab.com:47504/sandbox');
 
 // Configuration
-
 app.configure(function () {
   app.set('views', __dirname + '/views');
-  //app.set('view engine', 'jade');
+  app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
@@ -30,23 +28,10 @@ app.configure('production', function () {
 });
 
 // Routes
-
-//app.get('/', routes.index);
-app.get('/', function (request, response) {
-  response.send('GET');
-});
-
-app.post('/', function (request, response) {
-  response.send('POST');
-});
-
-app.put('/', function (request, response) {
-  response.send('PUT');
-});
-
-app.delete('/', function (request, response) {
-  response.send('DELETE');
-});
+app.get('/', routes.routeGetIndex);
+app.post('/', routes.routeGetIndex);
+app.put('/', routes.routeGetIndex);
+app.delete('/', routes.routeGetIndex);
 
 app.listen(port, function () {
   console.log("Express server listening on port %d in %s mode", port, app.settings.env);
