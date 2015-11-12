ec90d03f070891163d7063fd1c590dfae901fb11
Maksim Tarhunakau <BLRplex@gmail.com>
2015-11-12 11:35:58 +0300

    Webpack config normalizing, requires REBUILDING PACKAGE.JSON

--- angular-spa-ui/webpack.config.js
+++ angular-spa-ui/webpack.config.js
@@ -1,59 +1,56 @@
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
+var path = require('path');
+var AngularPlugin = require('angular-webpack-plugin');
 
 var ENV_DEV = process.env.NODE_ENV === 'Development';
 
 module.exports = {
-    context: __dirname,
-    entry: {
-        /*common: [ // Temporary removed
-            './js/ui-core/common.js'
-        ],*/
-        vendor: [
-            'angular',
-            'angular-ui-router',
-            'underscore'
-        ],
-        admin: './js/admin-app/admin-app.js',
-        search: './js/search-app/search-app.js',
-    },
-    output: {
-        path: __dirname + '/build/scripts/',
-        filename: '[name].bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.sass$/,
-                loader: ExtractTextPlugin.extract("css!sass")
-            }
-        ]
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            _: 'underscore'//,
-            //angular: 'angular',
-           // 'window.angular': 'angular'
-        }),
-        new CommonsChunkPlugin('vendor', 'vendor.js'),
-        new ExtractTextPlugin('../styles/styles.css', {
-            allChunks: true
-        })
-    ],
-    watch: ENV_DEV,
-    watchOptions: {
-        aggregateTimeout: 100
-    },
-    devtool: (ENV_DEV ? 'source-map' : null)
+  context: __dirname,
+  entry: {
+    search: path.resolve(__dirname, './js/search-app/search-app.js'),
+    admin: path.resolve(__dirname, './js/admin-app/admin-app.js'),
+    vendor: [
+      'underscore',
+      'angular-ui-router'
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, './build/scripts'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.sass$/,
+        loader: ExtractTextPlugin.extract("css!sass")
+      }
+    ]
+  },
+  plugins: [
+    new AngularPlugin(),
+    new webpack.ProvidePlugin({
+      _: 'underscore',
+    }),
+    new CommonsChunkPlugin('vendor', 'vendor.js'),
+    new ExtractTextPlugin('../styles/styles.css', {
+      allChunks: true
+    })
+  ],
+  watch: ENV_DEV,
+  watchOptions: {
+    aggregateTimeout: 100
+  },
+  devtool: (ENV_DEV ? 'source-map' : null)
 };
 
 if (!ENV_DEV) {
-    module.exports.plugins.push(new webpack.optimize.UglifyJsPlugin({
-        compress: {
-            warnings: false
-        },
-        mangle: false,
-        exclude: /.\.min\.js$/i
-    }));
+  module.exports.plugins.push(new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    },
+    mangle: false,
+    exclude: /.\.min\.js$/i
+  }));
 }
