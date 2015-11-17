57946c741aff5152aa75d6ec37af98a4e1555d4f
Maksim Tarhunakau <BLRplex@gmail.com>
2015-11-17 09:06:19 +0300

    Using path aliases and redirecting for base pages

--- rest-modules/admin/index.js
+++ rest-modules/admin/index.js
@@ -1,6 +1,12 @@
 module.exports = function (app) {
-    // Defining routes
-    var routes = require('./routes.js');
+    app.get('/admin', function (request, response) {
+        response.redirect('/admin-app');
+    });
 
-    app.get('/admin', routes.getAdminHome);
+    app.get('/admin-app', function (request, response) {
+        response.render(__dirname + '/views/index.jade', {
+            title: 'Express',
+            content: 'That is a GET request'
+        });
+    });
 }
--- rest-modules/admin/routes.js
+++
@@ -1,6 +0,0 @@
-exports.getAdminHome = function (request, response) {
-    response.render(__dirname + '/views/index.jade', {
-        title: 'Express',
-        content: 'That is a GET request'
-    });
-}
