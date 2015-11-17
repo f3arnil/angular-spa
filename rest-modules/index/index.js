b989bb15a88162ab84efc8d3ce47eed7648d761e
Maksim Tarhunakau <BLRplex@gmail.com>
2015-11-17 14:45:50 +0300

    Base search API

--- rest-modules/index/index.js
+++ rest-modules/index/index.js
@@ -2,12 +2,4 @@ module.exports = function (app) {
     app.get('/', function (request, response) {
         response.redirect('/search-app');
     });
-
-    app.get('/search-app', function (request, response) {
-        response.render('js/search-app/index.jade', {
-            way: 'js/search-app/',
-            backEndData: 'That is a message from Backend',
-            userId: app.envSettings.user
-        });
-    });
 }
