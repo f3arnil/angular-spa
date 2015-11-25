d6af303979b689213f1203834da2401342bb5033
Alex Vasilyeu <f3arnil@gmail.com>
2015-11-25 09:49:23 +0300

    Search test with results

--- angular-spa-ui/js/search-app/search-module/search-ctrl.js
+++ angular-spa-ui/js/search-app/search-module/search-ctrl.js
@@ -3,14 +3,30 @@
 module.exports = function (search) {
     
     search.controller('searchCtrl', function ($scope, $http, appConfig) {
+        $scope.query = '';
+        $scope.queryOffset = 0;
+        $scope.queryLimit = 15;
+        $scope.querySortBy = 'DESC';
+        $scope.searchPlace = 'Publications';
+        $scope.queryResult = '';
+        $scope.showResults = false;
         $scope.greeting = 'Hello world from search simple!';
         var config = appConfig.config;
-        var data = 
-            $http({ method : config.methods.GET, url : '/service/search/?query=1&offset=0&limit=15&sortBy=DESC'})
+
+        $scope.find = function () {
+            $scope.showResults = false;
+            var query = $scope.query,
+                offset = $scope.queryOffset,
+                limit = $scope.queryLimit,
+                sortBy = $scope.querySortBy;
+            
+            $http({ method : config.methods.GET, url : '/service/search/?query='+query+'&offset='+offset+'&limit='+limit+'&sortBy='+sortBy})
         .success(function (data) {
-                    console.log(data);
-                });
-        
+                $scope.queryResult = data.data.publication.items;
+                $scope.showResults = true;
+                console.log($scope.queryResult);
+            });
+        };
     });
     
     search.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, items) {
