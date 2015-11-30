f09fd7493a2d78bfa5797e7836851c12c05186da
Alex Vasilyeu <f3arnil@gmail.com>
2015-11-30 14:19:54 +0300

    Search simple

--- angular-spa-ui/js/search-app/search-module/search-ctrl.js
+++ angular-spa-ui/js/search-app/search-module/search-ctrl.js
@@ -1,63 +1,89 @@
 "use strict";
 
+// to do 
+// when some parametr changed put it in $scope.queryParams and use only $scope.find
+
 module.exports = function (search) {
 
     search.controller('searchCtrl', function ($scope, $http, searchConfig, $uibModal, $stateParams, $state, promises, queryParams, searchStorage) {
-        var config = searchConfig.config;
-        $scope.queryResult = '';
-        $scope.showResults = false;
-        $scope.queryParams = $stateParams;
-        $scope.sortParams = config.sortParams;
-        $scope.resultsPerPages = config.resultsPerPage;
-        $scope.searchInList = config.searchIn;
         
         //Change current searchIn
-        $scope.setSearchIn = function (val){
+        $scope.setSearchIn = function (val) {
             $scope.searchIn = $scope.searchInList[findValue(val, $scope.searchInList)];
-            $state.go(
-                'search.simpleQuery', 
-                { query: $scope.query, searchIn : $scope.searchIn.value }, 
-                { reload : true }
-            );
+            $scope.queryParams.searchIn = $scope.searchIn.value;
+            if ($scope.hasQuery()){
+                $state.go(
+                    'search.simpleQuery',
+                    $scope.queryParams,
+                    {
+                        reload : true
+                    }
+                );
+            }
+            
         };
         
         // Function to find smth
         $scope.find = function () {
-            $scope.showResults = false;
-            $stateParams = {};
-            $state.go(
-                'search.simpleQuery', 
-                { query: $scope.query, limit : $scope.limit.value, sortBy : $scope.sortBy.value, searchIn : $scope.searchIn.value }, 
-                { inherit : false, reload : true }
-            );
+            if ($scope.hasQuery()){
+                $scope.queryParams.query = $scope.query;
+                $scope.showResults = false;
+                $state.go(
+                    'search.simpleQuery',
+                    $scope.queryParams,
+                    {
+                        inherit : true,
+                        reload : true
+                    }
+                );
+            }
+            
         };
         
         //Action when sortBy changed
         $scope.sortChange = function () {
+            $scope.queryParams.sortBy = $scope.sortBy.value;
             $state.go(
-                'search.simpleQuery', 
-                { query: $scope.query, sortBy : $scope.sortBy.value }, 
-                { reload : true }
+                'search.simpleQuery',
+                $scope.queryParams,
+                {
+                    reload : true 
+                }
             );
         };
         
         //Action when limit changed
         $scope.limitChange = function () {
+            $scope.queryParams.limit = $scope.limit.value;
             $state.go(
-                'search.simpleQuery', 
-                { query: $scope.query, limit : $scope.limit.value }, 
-                { reload : false }
+                'search.simpleQuery',
+                $scope.queryParams,
+                {
+                    reload : true
+                }
             );
         };
 
         //Pagination change page
         $scope.goToPage = function () {
-            var offset = $scope.currentPage * $scope.limit.value - $scope.limit.value;
+            $scope.queryParams.offset = $scope.currentPage * $scope.limit.value - $scope.limit.value;
             $state.go(
-                'search.simpleQuery', 
-                { query: $scope.query, offset : offset }, 
-                { inherit : true }
+                'search.simpleQuery',
+                $scope.queryParams,
+                {
+                    inherit : true,
+                    reload : true
+                }
             );
+        };
+        
+        // Check has model "query" data or not - if yes returns true, else false
+        $scope.hasQuery = function () {
+            if ($scope.query) {
+                return true;
+            } else {
+                return false;
+            }
         };
         
         //Set results data to controllers values
@@ -77,7 +103,7 @@ module.exports = function (search) {
             searchStorage.params = $scope.queryParams;
         }
         
-        //Find value in objects list (for limits and sortBy)
+        //Find value in objects list (for limits and sortBy) and returns its id
         function findValue(val, object) {
             for (var x in object) {
                 if (object[x].value === val ) {
@@ -95,6 +121,7 @@ module.exports = function (search) {
             return resultsTo;
         }
 
+        //if object has no keys return true, else false
         function isEmptyObject(obj) {
             if (Object.keys(obj).length === 0) {
                 return true;
@@ -103,11 +130,31 @@ module.exports = function (search) {
             }
         }
 
+        //If query params is empty set it to default
+        function setDefaultParams() {
+            var params = {};
+            params.searchIn = $scope.searchInList[0].value;
+            params.limit = $scope.resultsPerPages[0].value;
+            params.sortBy = $scope.sortParams[0].value;
+            params.offset = '0';
+            params.orderBy = 'title';
+            params.query = '';
+            return params;
+        };
+        
+        var config = searchConfig.config;
+        $scope.queryResult = '';
+        $scope.showResults = false;
+        $scope.queryParams = $stateParams;
+        $scope.sortParams = config.sortParams;
+        $scope.resultsPerPages = config.resultsPerPage;
+        $scope.searchInList = config.searchIn;
+        
         if (isEmptyObject($scope.queryParams) || $scope.queryParams.query === undefined) {
-            $scope.queryParams = null;
-            $scope.sortBy = $scope.sortParams[0];
-            $scope.limit = $scope.resultsPerPages[0];
-            $scope.searchIn = $scope.searchInList[0];
+            $scope.queryParams = setDefaultParams();
+            $scope.sortBy = $scope.sortParams[findValue($scope.queryParams.sortBy, $scope.sortParams)];
+            $scope.limit = $scope.resultsPerPages[findValue($scope.queryParams.limit, $scope.resultsPerPages)];
+            $scope.searchIn = $scope.searchInList[findValue($scope.queryParams.searchIn, $scope.searchInList)];
             if (!isEmptyObject(searchStorage.data) && !isEmptyObject(searchStorage.params)) {
                 $scope.queryParams = searchStorage.params;
                 setCtrlData(searchStorage.data);
@@ -118,6 +165,7 @@ module.exports = function (search) {
             $scope.searchIn = $scope.searchInList[findValue($scope.queryParams.searchIn, $scope.searchInList)];
             promises.getAsyncData('GET', queryUrl)
             .then(function (result) {
+                console.log($scope.queryParams);
                 var publications = result.data[$scope.searchIn.value];
                 setCtrlData(publications);
             })
@@ -130,4 +178,5 @@ module.exports = function (search) {
             $state.go('search.advanced');
         };
     });
+
 };
