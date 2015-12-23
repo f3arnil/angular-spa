ad60933534b61cce7b49f29e7c53f1a58f6ed5f0
Alex Vasilev <f3arnil@gmail.com>
2015-12-23 17:49:47 +0300

    Fix

--- angular-spa-ui/js/common/recordsListDir/index.js
+++ angular-spa-ui/js/common/recordsListDir/index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 module.exports = function (parentModule) {
-    
+
     var recordsListDir = require('./recordsList-dir'),
         recordsListSrv = require('./recordsList-srv'),
         recordsListCtrl = require('./recordsList-ctrl');
@@ -11,27 +11,4 @@ module.exports = function (parentModule) {
         .directive('recordsList', recordsListDir)
         .service('rlService', recordsListSrv)
 
-
-    parentModule.provider('test', function (recordsListConfig) {
-        var thisIsPrivate = recordsListConfig;
-        console.log('!', recordsListConfig);
-        return {
-
-            setPrivate: function (newVal) {
-                thisIsPrivate = newVal;
-            },
-
-            $get: function () {
-                function getPrivate() {
-                    return thisIsPrivate;
-                }
-
-                return {
-                    variable: "This is public",
-                    getPrivate: getPrivate
-                };
-            }
-
-        };
-    });
 };
